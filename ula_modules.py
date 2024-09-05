9f1b98a5ec5b0dc30a5514218bc1732f6e1d0dc4
Alec <100847675+alec302@users.noreply.github.com>
2024-09-05 13:00:50 +0000

    a2 ha fa

--- ula_modules.py
+++ ula_modules.py
@@ -11,22 +11,35 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
-        pass
+        soma.next = (a and not b) or (not a and b)
+        carry.next = (a and b)
 
     return instances()
 
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+
+    half_1 = halfAdder(a, b, s[0], s[1]) 
+    half_2 = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2]
 
     return instances()
 
 
 @block
 def adder2bits(x, y, soma, carry):
+
+    c0 = Signal(bool(0))
+
+    ha = halfAdder(x[0], y[0], soma[0], c0)
+    fa = fullAdder(x[1], y[1], c0, soma[1], carry)
+
+
     return instances()
 
 
@@ -34,6 +47,6 @@ def adder2bits(x, y, soma, carry):
 def adder(x, y, soma, carry):
     @always_comb
     def comb():
-        pass
+        
 
     return instances()
